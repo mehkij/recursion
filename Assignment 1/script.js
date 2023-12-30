aa25a1274dec328de4ee257815d1ec695652e42d
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-12-30 14:44:46 -0500

    Add recursive solution

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -14,3 +14,17 @@ function fibs(n) {
 };
 
 console.log(fibs(8));
+
+function fibsRec(n, arr = []) {
+  if (n === 1) {
+    arr.push(0);
+  } else if (n === 2) {
+    arr.push(0, 1);
+  } else {
+    fibsRec(n - 1, arr);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+  }
+}
+
+console.log(fibsRec(8));
